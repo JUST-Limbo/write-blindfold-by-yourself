99c02d34a5585602be607041d1dc9812985bb07a
Limbo <1034935762@qq.com>
2021-05-25 20:29:21 +0800

    2021年5月25日20:29:20

--- JavaScript/数组API/重排序方法/sort.js
+++ JavaScript/数组API/重排序方法/sort.js
@@ -1,3 +1,12 @@
+// sort() 方法用原地算法对数组的元素进行排序，并返回数组。默认排序顺序是在将元素转换为字符串，然后比较它们的UTF-16代码单元值序列时构建的
+/*
+  如果指明了 compareFunction ，那么数组会按照调用该函数的返回值排序。即 a 和 b 是两个将要被比较的元素：
+  如果 compareFunction(a, b) 小于 0 ，那么 a 会被排列到 b 之前；
+  如果 compareFunction(a, b) 等于 0 ， a 和 b 的相对位置不变。备注： ECMAScript 标准并不保证这一行为，而且也不是所有浏览器都会遵守（例如 Mozilla 在 2003 年之前的版本）；
+  如果 compareFunction(a, b) 大于 0 ， b 会被排列到 a 之前。
+  compareFunction(a, b) 必须总是对相同的输入返回相同的比较结果，否则排序的结果将是不确定的。
+*/
+
 Array.prototype.mySort = function (compareFun) {
 	if (typeof compareFun == 'function' || typeof compareFun == 'undefined') {
 		for (let i = 0; i < this.length; i++) {
